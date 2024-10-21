1704091f10d2e62494e61f8dc5b25ba1fd6cc264
Henok Yoseph <henok.y2014@gmail.com>
2024-10-21 08:05:43 +0300

    first commit

---
+++ Day81TextToMorseCode/main.py
@@ -0,0 +1,4 @@
+from textToMorse import TextToMorse
+
+text_to_morse = TextToMorse('I am a python programmer')
+text_to_morse.convert_to_morse()
---
+++ Day81TextToMorseCode/textToMorse.py
@@ -0,0 +1,34 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                       'C': '-.-.', 'D': '-..', 'E': '.',
+                       'F': '..-.', 'G': '--.', 'H': '....',
+                       'I': '..', 'J': '.---', 'K': '-.-',
+                       'L': '.-..', 'M': '--', 'N': '-.',
+                       'O': '---', 'P': '.--.', 'Q': '--.-',
+                       'R': '.-.', 'S': '...', 'T': '-',
+                       'U': '..-', 'V': '...-', 'W': '.--',
+                       'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                       '1': '.----', '2': '..---', '3': '...--',
+                       '4': '....-', '5': '.....', '6': '-....',
+                       '7': '--...', '8': '---..', '9': '----.',
+                       '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                       '?': '..--..', '/': '-..-.', '-': '-....-',
+                       '(': '-.--.', ')': '-.--.-'}
+class TextToMorse:
+
+    def __init__(self,text):
+        self.text = text.upper()
+
+    def convert_to_morse(self):
+        morse_code = ''
+
+        for word in self.text:
+            if word == ' ':
+                morse_code = morse_code + ' '
+                continue
+            for (key,value) in MORSE_CODE_DICT.items():
+                if word == key:
+                    morse_code = morse_code + value
+                    continue
+
+        print(morse_code)
+
